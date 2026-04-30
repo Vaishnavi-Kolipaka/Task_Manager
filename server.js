eca7cbccb0eb1f030edfc80202a908f89326c68d
Vaishnavi-Kolipaka <kolipakavaishnavi7@gmail.com>
2026-04-30 13:52:11 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors({
+    origin:"https://taskmanagervk.netlify.app",
+    methods: ['GET','POST','PUT','DELETE'],
+    credentials: true
+}));
+app.use(express.json());
+
+// Routes
+app.get("/",(req,res) => {
+    res.send("Backend is running");
+});
+app.use("/auth", require("./routes/auth"));
+app.use("/tasks", require("./routes/tasks"));
+app.use("/projects", require("./routes/projects"));
+
+// DB connect
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
